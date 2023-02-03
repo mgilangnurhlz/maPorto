e9b90e09fd6d303775a8ced217ce42d7a1cbf7ff
Muhammad Gilang Nur Haliz <mgilangnurhaliz@gmail.com>
2023-02-03 07:49:21 +0700

    navbar

---
+++ script2.js
@@ -0,0 +1,37 @@
+function myFunction(x) {
+  if (x.matches) {
+    window.onscroll = function () {
+      scrollFunction();
+    };
+
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 170 ||
+        document.documentElement.scrollTop > 170
+      ) {
+        document.getElementById("navbar").style.top = "0";
+      } else {
+        document.getElementById("navbar").style.top = "-70px";
+      }
+    }
+  } else {
+    window.onscroll = function () {
+      scrollFunction();
+    };
+
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 390 ||
+        document.documentElement.scrollTop > 390
+      ) {
+        document.getElementById("navbar").style.top = "0";
+      } else {
+        document.getElementById("navbar").style.top = "-70px";
+      }
+    }
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction);
